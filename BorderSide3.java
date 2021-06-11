838f0a6610fa8257299353787319390cbac5bb92
Zrotix <joskim24@bergen.org>
2021-06-11 10:15:19 -0400

    Added ghost spawn, fixed some borders

---
+++ BorderSide3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BorderSide3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BorderSide3 extends Border
+{
+    /**
+     * Act - do whatever the BorderSide3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ BorderSide4.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BorderSide4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BorderSide4 extends Border
+{
+    /**
+     * Act - do whatever the BorderSide4 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
